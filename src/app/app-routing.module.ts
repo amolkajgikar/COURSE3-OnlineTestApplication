260a86b421836d97fd28ad1c5b84853de0fbaaae
Amol Kajgikar <amolkajgikar@gmail.com>
2022-02-21 23:19:46 +0400

    initial version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { QuestionComponent } from './question/question.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'welcome',pathMatch:"full"},
+  {path:"welcome", component:WelcomeComponent },
+  {path:"questions", component:QuestionComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
